e3363d5e1adab4b9a6082f87db2897011a1a70b4
Jared Boynton <jared.boynton@postman.com>
2025-09-20 11:21:28 -0400

    cleanup

--- test_fix.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "Testing GitHub gist storage initialization fix..."
-
-# Run the specific test
-cargo test github_gist_storage::tests::test_gist_storage_initialization --lib
-
-echo "Test completed."
